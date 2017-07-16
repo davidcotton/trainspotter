17902c8eaa37e50d1bec1bbf1dc93af3925753db
Dave Cotton <david.f.cotton@gmail.com>
2017-07-16 21:08:08 +1000

    Fixed GenreService tests

--- test/services/GenreServiceTest.java
+++ test/services/GenreServiceTest.java
@@ -7,15 +7,19 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import io.atlassian.fugue.Either;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import models.Genre;
+import models.Genre.Status;
 import models.create.CreateGenre;
+import models.update.UpdateGenre;
 import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,7 +28,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import play.data.Form;
-import play.data.FormFactory;
 import repositories.GenreRepository;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -32,9 +35,8 @@ public class GenreServiceTest {
   
   @InjectMocks private GenreService genreService;
   @Mock private GenreRepository mockGenreRepository;
-  @Mock private FormFactory mockFormFactory;
-  @Mock private Form mockForm;
-  @Mock private Form mockDataForm;
+  @Mock private Form<CreateGenre> mockCreateGenreForm;
+  @Mock private Form<UpdateGenre> mockUpdateGenreForm;
 
   @Test public void fetchAll() {
     when(mockGenreRepository.findAll()).thenReturn(new ArrayList<Genre>() {{
@@ -101,86 +103,89 @@ public class GenreServiceTest {
     assertThat(maybeGenre.isPresent(), is(false));
   }
 
-//  @Test public void insert_successGivenValidData() {
-//    // ARRANGE
-//    Form<CreateGenre> genreForm = new Form<>(CreateGenre.class, null, null, null);
-//
-//    when(genreForm.hasErrors()).thenReturn(false);
-//
-//    // ACT
-//    Either<Form<CreateGenre>, Genre> genreOrError = genreService.insert(genreForm);
-//
-//    // ASSERT
-//    // assert left (error value) is not present
-//    assertFalse(genreOrError.isLeft());
-//    // assert right (success value) is present
-//    assertTrue(genreOrError.isRight());
-//    assertThat(genreOrError.right().get(), instanceOf(Genre.class));
-//    // verify that the user repository inserted the new user
-//    ArgumentCaptor<Genre> argument = ArgumentCaptor.forClass(Genre.class);
-//    verify(mockGenreRepository).insert(argument.capture());
-//    assertThat(argument.getValue().getName(), is("Techno"));
-//    assertThat(argument.getValue().getSlug(), is("techno"));
-//  }
-//
-//  @Test
-//  public void insert_failureGivenInvalidData() {
-//    // ARRANGE
-//    Genre genre = new Genre(null, null, null, null, null, null);
-//
-//    Map<String, List<ValidationError>> validationErrors =
-//        new HashMap<String, List<ValidationError>>() {{
-//          put("name", mock(List.class));
-//        }};
-//
-//    when(mockFormFactory.form(Genre.class, Genre.InsertValidators.class)).thenReturn(mockDataForm);
-//    when(mockDataForm.bind(any(JsonNode.class))).thenReturn(mockForm);
-//    when(mockForm.hasErrors()).thenReturn(true);
-//    when(mockForm.errors()).thenReturn(validationErrors);
-//
-//    // ACT
-//    Either<Map<String, List<ValidationError>>, Genre> genreOrError = genreService.insert(genre);
-//
-//    // ASSERT
-//    // assert right (success value) is not present
-//    assertFalse(genreOrError.isRight());
-//    // assert left (error value) is present
-//    assertTrue(genreOrError.isLeft());
-//    assertThat(genreOrError.left().get().get("name"), instanceOf(List.class));
-//    // verify that the genreRepository never tried to insert the invalid genre
-//    verify(mockGenreRepository, never()).insert(any());
-//  }
-//
-//  @Test
-//  public void update_successGivenValidData() {
-//    // ARRANGE
-//    Genre savedGenre = new Genre(
-//        1L, "Techno", new ArrayList<>(), new ArrayList<>(), ZonedDateTime.now(),
-//        ZonedDateTime.now()
-//    );
-//    Genre newGenre = new Genre(
-//        1L, "Deep House", new ArrayList<>(), new ArrayList<>(), ZonedDateTime.now(),
-//        ZonedDateTime.now()
-//    );
-//
-//    when(mockFormFactory.form(Genre.class, Genre.UpdateValidators.class))
-//        .thenReturn(mockDataForm);
-//    when(mockDataForm.bind(any(JsonNode.class))).thenReturn(mockForm);
-//    when(mockForm.hasErrors()).thenReturn(false);
-//
-//    // ACT
-//    Either<Map<String, List<ValidationError>>, Genre> genreOrError = genreService
-//        .update(savedGenre, newGenre);
-//
-//    // ASSERT
-//    // assert left (error value) is not present
-//    assertFalse(genreOrError.isLeft());
-//    // assert right (success value) is present
-//    assertTrue(genreOrError.isRight());
-//    // verify that the user repository updated the user
-//    ArgumentCaptor<Genre> argument = ArgumentCaptor.forClass(Genre.class);
-//    verify(mockGenreRepository).update(argument.capture());
-//    assertThat(argument.getValue().getName(), is("Deep House"));
-//  }
+  @Test public void insert_success() {
+    // ARRANGE
+    CreateGenre createGenre = new CreateGenre("House");
 
+    when(mockCreateGenreForm.hasErrors()).thenReturn(false);
+    when(mockCreateGenreForm.get()).thenReturn(createGenre);
+
+    // ACT
+    Either<Form<CreateGenre>, Genre> genreOrError = genreService.insert(mockCreateGenreForm);
+
+    // ASSERT
+    // assert left (error value) is not present
+    assertFalse(genreOrError.isLeft());
+    // assert right (success value) is present
+    assertTrue(genreOrError.isRight());
+    assertThat(genreOrError.right().get(), instanceOf(Genre.class));
+    // verify that the genre repository inserted the new genre
+    ArgumentCaptor<Genre> argument = ArgumentCaptor.forClass(Genre.class);
+    verify(mockGenreRepository).insert(argument.capture());
+    assertThat(argument.getValue().getName(), is("House"));
+  }
+
+  @Test public void insert_failureWhenFailsValidation() {
+    // ARRANGE
+    when(mockCreateGenreForm.hasErrors()).thenReturn(true);
+    Genre mockGenre = mock(Genre.class);
+
+    // ACT
+    Either<Form<CreateGenre>, Genre> genreOrError = genreService.insert(mockCreateGenreForm);
+
+    // ASSERT
+    // assert right (success value) is not present
+    assertFalse(genreOrError.isRight());
+    // assert left (error value) is present
+    assertTrue(genreOrError.isLeft());
+    // verify that the genreRepository never tried to insert the invalid genre
+    verify(mockGenreRepository, never()).insert(mockGenre);
+  }
+
+  @Test public void update_success() {
+    // ARRANGE
+    Genre savedGenre = new Genre(
+        1L, "Tech House", "tech-house", null, null,
+        Status.active, ZonedDateTime.now(), ZonedDateTime.now()
+    );
+    UpdateGenre updateGenre = new UpdateGenre("Deep House");
+
+    when(mockUpdateGenreForm.hasErrors()).thenReturn(false);
+    when(mockUpdateGenreForm.get()).thenReturn(updateGenre);
+
+    // ACT
+    Either<Form<UpdateGenre>, Genre> genreOrError = genreService.update(savedGenre, mockUpdateGenreForm);
+
+    // ASSERT
+    // assert left (error value) is not present
+    assertFalse(genreOrError.isLeft());
+    // assert right (success value) is present
+    assertTrue(genreOrError.isRight());
+    // verify that the user repository updated the user
+    ArgumentCaptor<Genre> argument = ArgumentCaptor.forClass(Genre.class);
+    verify(mockGenreRepository).update(argument.capture());
+    assertThat(argument.getValue().getName(), is("Deep House"));
+  }
+
+  @Test public void update_failureWhenFailsValidation() {
+    // ARRANGE
+    Genre savedGenre = new Genre(
+        1L, "Tech House", "tech-house", null, null,
+        Status.active, ZonedDateTime.now(), ZonedDateTime.now()
+    );
+    Genre mockGenre = mock(Genre.class);
+
+    when(mockUpdateGenreForm.hasErrors()).thenReturn(true);
+
+    // ACT
+    Either<Form<UpdateGenre>, Genre> genreOrError = genreService.update(savedGenre, mockUpdateGenreForm);
+
+    // ASSERT
+    // assert right (success value) is not present
+    assertFalse(genreOrError.isRight());
+    // assert left (error value) is present
+    assertTrue(genreOrError.isLeft());
+    // verify that the genreRepository never tried to insert the invalid genre
+    verify(mockGenreRepository, never()).update(mockGenre);
+  }
 }
